b9447e651890777c987926a64f670758a028f739
Marc Aschmann <marc@aschmann.org>
2015-04-05 22:31:12 +0200

    switched to controller-as-a-service, moved core services to general definition app/*

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# changelog
+
+## v2.2.0
+
+* BC break: removed BaseController, replaced with trait
+* Added abstract to implement trait and give base for abstract service definition
--- src/CoreBundle/Controller/HomeController.php
+++ src/CoreBundle/Controller/HomeController.php
@@ -1,23 +1,21 @@
 <?php
-
 namespace CoreBundle\Controller;
 
-use Core\Controller\BaseController as Controller;
+use Core\Controller\BaseServiceController;
 
 /**
  * Class HomeController
  *
  * @package CoreBundle\Controller
  */
-class HomeController extends Controller
+class HomeController extends BaseServiceController
 {
-
     /**
      * @return mixed
      */
     public function indexAction()
     {
-        return $this->render(
+        return $this->templating->renderResponse(
             $this->getTemplatePath(),
             array()
         );
